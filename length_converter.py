b15adeaf32c05f808369bfd893a03e7e605bf81d
Janis Löns <janis.loens@outlook.de>
2025-06-18 13:26:24 +0200

    Added all

--- length_converter.py
+++ length_converter.py
@@ -1,6 +1,11 @@
-def meters_to_kilometers(meters):
+def meters_to_kilometers(meters) -> float:
     """
     Convert meters to kilometers.
+
+    Args:
+    meters (int or float): The length in meters to convert.
+    Returns:
+    float: The length in kilometers.
     """
     kilometers = meters / 1000
     return kilometers
